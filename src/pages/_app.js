eba54f3d8f50a68d89d3659b5ef11c333b478bb9
Azmain Morshed <114017420+azmainm@users.noreply.github.com>
2024-08-28 00:28:43 +0600

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,18 @@
 // src/pages/_app.js
 
 import '../styles/globals.css';  // Import global styles
+import Head from 'next/head';
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/images/logo_s.png" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
