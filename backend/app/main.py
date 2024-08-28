eae348fa59b8d44d5ba1dc95d2843a7e3d376bb7
Azmain Morshed <114017420+azmainm@users.noreply.github.com>
2024-08-28 09:32:47 +0600

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -335,4 +335,5 @@ Base.metadata.create_all(bind=engine)
 if __name__ == "__main__":
     import uvicorn
     # uvicorn.run(app, host="127.0.0.1", port=8000, reload=True)
-    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
+    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
+    # uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
