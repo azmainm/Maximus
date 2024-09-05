8314fc72ec70f4a72947be989b0708d0d3c1b04e
Azmain Morshed <114017420+azmainm@users.noreply.github.com>
2024-09-05 13:45:28 +0600

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -29,7 +29,7 @@ ACCESS_TOKEN_EXPIRE_MINUTES = 30
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
 origins = [
-    "https://maximus-cedrrsdq0-azmain-morsheds-projects.vercel.app",
+    "https://maximus-idowyzhxw-azmain-morsheds-projects.vercel.app",
     "https://maximus-phi.vercel.app",
 ]
 
